e34db57f79e3cda8f303da30ee96a1a80f184511
Fernandof <fittipaldifernando@gmail.com>
2024-06-09 03:18:55 -0300

    fixed searchMusicians

--- src/views/Musicians.vue
+++ src/views/Musicians.vue
@@ -4,7 +4,9 @@
     <button @click="searchMusicians">Buscar Músicos</button>
     <ul>
       <li v-for="musician in musicians" :key="musician.id">
-        {{ musician }}
+        <p>Nombre: {{ musician.name }}</p>
+        <p>Apellido: {{ musician.lastName }}</p>
+        <p>Email: {{ musician.email }}</p>
       </li>
     </ul>
   </div>
@@ -19,7 +21,8 @@ const musicians = ref([]);
 const searchMusicians = async () => {
   try {
     const response = await axios.get('http://localhost:8080/musicians');
-    musicians.value = response.data;
+    musicians.value = response.data.message;
+    console.log(musicians.value);
   } catch (error) {
     console.error('Error buscando músicos:', error);
   }
@@ -42,4 +45,13 @@ button {
 button:hover {
   background-color: #555;
 }
+
+li {
+  margin-bottom: 10px;
+}
+
+p {
+  margin: 0;
+}
+
 </style>
